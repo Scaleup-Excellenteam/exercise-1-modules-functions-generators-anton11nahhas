86538e3ee840121efd09115186983acbddac11f4
Anton Nahhas <antonna@edu.hac.ac.il>
2023-05-04 15:50:06 +0300

    resubmitting the code, added consts,a main function and a new function to test if the directory given is valid.

--- week1/week1-5.1.1-the_way.py
+++ week1/week1-5.1.1-the_way.py
@@ -9,11 +9,34 @@ def the_way(path):
         :returns: a list of files that accomplish the goal of the function
     """
 
+    WANTED_WORD = 'deep'
+    FIRST_FOUR_LETTERS = 4
+
     result_lst = []
     for file_name in os.listdir(path):
-        if file_name[:4].lower() == "deep":
+        if file_name[:FIRST_FOUR_LETTERS].lower() == WANTED_WORD:
             result_lst.append(file_name)
     return result_lst
 
 
-the_way(input("Please enter a path:"))
+def is_valid_directory(path):
+    """This function receives a directory path and checks if it exists and is a directory.
+
+    :param path: The directory path to check.
+    :returns: True if the path is a valid directory, False otherwise.
+    """
+    return os.path.isdir(path)
+
+def main():
+    ERROR_MSG = 'The path you provided is invalid'
+    INPUT_MESSAGE = 'Please enter a path: '
+    given_dir = input(INPUT_MESSAGE)
+    if is_valid_directory(given_dir):
+        the_way(given_dir)
+    else:
+        print(ERROR_MSG)
+
+if __name__ == '__main__':
+    main()
+
+
