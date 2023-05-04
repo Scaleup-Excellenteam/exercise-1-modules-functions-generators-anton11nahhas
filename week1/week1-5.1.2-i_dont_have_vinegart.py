6229c8e8eb20dfd2ab9f7d4db6c588a698affa2a
Anton Nahhas <antonna@edu.hac.ac.il>
2023-05-04 16:06:15 +0300

    resubmitting the code, added consts,and a main function.

--- week1/week1-5.1.2-i_dont_have_vinegart.py
+++ week1/week1-5.1.2-i_dont_have_vinegart.py
@@ -2,7 +2,7 @@ import random
 import datetime
 
 
-def rand_year(first, second):
+def rand_year(first_date, second_date):
     """This function takes two dates as a range, finds a random date
         between those two dates, and returns it
 
@@ -11,18 +11,25 @@ def rand_year(first, second):
         :returns: a random date inside that date
     """
 
-    time_range = second - first
+    time_range = second_date - first_date
     days_range = time_range.days
-    rand_day = random.randrange(days_range)
-    rand_date = first + datetime.timedelta(days=rand_day)
-    return rand_date
+    random_day = random.randrange(days_range)
+    random_date = first_date + datetime.timedelta(days=random_day)
+    return random_date
+
+def main():
+    NO_VINEGAR_MSG = "I DON'T HAVE VINEGART"
+    MONDAY_MSG = "IT'S NOT MONDAY"
+    MONDAY = 0
+    first_day = input("Please enter a date in this format YYYY-MM-DD:").split('-')
+    second_day = input("Please enter another date in this format YYYY-MM-DD:").split('-')
+    first_date = datetime.date(int(first_day[0]), int(first_day[1]), int(first_day[2]))
+    second_date = datetime.date(int(second_day[0]), int(second_day[1]), int(second_day[2]))
+    if rand_year(first_date, second_date).weekday() == MONDAY:
+        print(NO_VINEGAR_MSG)
+    else:
+        print(MONDAY_MSG)
 
 
-first = input("Please enter a date in this format YYYY-MM-DD:").split('-')
-second = input("Please enter another date in this format YYYY-MM-DD:").split('-')
-first_date = datetime.date(int(first[0]), int(first[1]), int(first[2]))
-second_date = datetime.date(int(second[0]), int(second[1]), int(second[2]))
-if rand_year(first_date, second_date).weekday() == 0:
-    print("I DON'T HAVE VINEGAR!")
-else:
-    print("its not monday..")
+if __name__ == '__main__':
+    main()
